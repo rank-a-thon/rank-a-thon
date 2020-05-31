b0da447f02035f4235c8aa18b00fbe35f32820c3
Christopher Goh <chrisgzf@gmail.com>
2020-06-01 02:37:32 +0800

    Add lint and prettier configuration

---
+++ web/prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+};
+
