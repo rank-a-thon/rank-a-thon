68dafbaa5e5903399877a1453084ac203b954606
Christopher Goh <chrisgzf@gmail.com>
2020-06-29 02:46:04 +0800

    Add announcements view

--- web/src/pages/announcements.tsx
+++ web/src/pages/announcements.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { NextPage } from 'next';
 import { Segment, Item } from 'semantic-ui-react';
 import MobilePostAuthContainer from '../components/MobilePostAuthContainer';
+import announcements from '../data/announcements.json';
 
 type PageProps = {
   getWidth?: () => number;
@@ -11,42 +12,24 @@ const DashboardLayout: NextPage<PageProps> = () => {
   const paragraph = 'Hey';
   return (
     <MobilePostAuthContainer title="Announcements">
-      <Item.Group link>
-        <Item>
-          <Item.Image
-            size="tiny"
-            src="https://react.semantic-ui.com/images/avatar/large/stevie.jpg"
-          />
-
-          <Item.Content>
-            <Item.Header>Stevie Feliciano</Item.Header>
-            <Item.Description>{paragraph}</Item.Description>
-          </Item.Content>
-        </Item>
-
-        <Item>
-          <Item.Image
-            size="tiny"
-            src="https://react.semantic-ui.com/images/avatar/large/veronika.jpg"
-          />
-
-          <Item.Content>
-            <Item.Header>Veronika Ossi</Item.Header>
-            <Item.Description>{paragraph}</Item.Description>
-          </Item.Content>
-        </Item>
-
-        <Item>
-          <Item.Image
-            size="tiny"
-            src="https://react.semantic-ui.com/images/avatar/large/jenny.jpg"
-          />
-
-          <Item.Content>
-            <Item.Header>Jenny Hess</Item.Header>
-            <Item.Description>{paragraph}</Item.Description>
-          </Item.Content>
-        </Item>
+      <Item.Group divided>
+        {announcements.map((announcement, index) => (
+          <Item key={index} style={{ padding: '0' }}>
+            <Item.Content
+              style={{
+                padding: '1.5em',
+                backgroundColor:
+                  announcement.level === 'urgent' ? 'pink' : null,
+              }}
+            >
+              <Item.Header>{announcement.subject}</Item.Header>
+              <Item.Meta>
+                {announcement.time} by {announcement.author}
+              </Item.Meta>
+              <Item.Description>{announcement.body}</Item.Description>
+            </Item.Content>
+          </Item>
+        ))}
       </Item.Group>
     </MobilePostAuthContainer>
   );
