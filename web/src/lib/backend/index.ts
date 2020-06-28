ed527915ddc9e1a891526fe0a191533d48f38d99
Christopher Goh <chrisgzf@gmail.com>
2020-06-28 23:29:51 +0800

    Allow flexible backend API endpoints

--- web/src/lib/backend/index.ts
+++ web/src/lib/backend/index.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-const API_URL = process.env.API_URL;
+let API_URL = process.env.API_URL;
 
 function getFullEndpoint(name: string): string {
+  if (window !== undefined) {
+    // Client only code
+    API_URL = `${location.protocol}//${window.location.host}/api/`;
+  }
   return `${API_URL}${name}`;
 }
 
