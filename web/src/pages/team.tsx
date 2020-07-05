f70852627e0429e9bb18c7c7d96233b84af20b8b
Christopher Goh <chrisgzf@gmail.com>
2020-07-05 12:58:55 +0800

    Add team UI for created teams

--- web/src/pages/team.tsx
+++ web/src/pages/team.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import type { NextPage } from 'next';
 import Router from 'next/router';
-import { Segment, Input, Button } from 'semantic-ui-react';
+import { Segment, Input, Button, List, Message } from 'semantic-ui-react';
 import { makeAuthedBackendRequest } from '../lib/backend';
 import MobilePostAuthContainer from '../components/MobilePostAuthContainer';
+import { getMe } from '../data/me';
 
 type PageProps = {
   getWidth?: () => number;
@@ -13,7 +14,7 @@ const DashboardLayout: NextPage<PageProps> = () => {
   const [teamName, setTeamName] = useState<string | null>(null);
   const [createTeamName, setCreateTeamName] = useState<string | null>(null);
 
-  async function getTeamName() {
+  async function getTeamNameAndMembers() {
     let response;
     try {
       response = await makeAuthedBackendRequest('get', 'v1/user');
@@ -32,6 +33,9 @@ const DashboardLayout: NextPage<PageProps> = () => {
     } catch (err) {
       console.log(err);
     }
+    const teamName = response.data.data.team_name;
+    const teamMemberIds = JSON.parse(response.data.data.user_ids);
+    // TODO: make this return team member names as well
     return response.data.data.team_name;
   }
 
@@ -49,7 +53,7 @@ const DashboardLayout: NextPage<PageProps> = () => {
   }
 
   useEffect(() => {
-    getTeamName().then((teamName) => setTeamName(teamName));
+    getTeamNameAndMembers().then((teamName) => setTeamName(teamName));
   }, []);
   return (
     <MobilePostAuthContainer title="Team" requireAuth>
@@ -59,9 +63,85 @@ const DashboardLayout: NextPage<PageProps> = () => {
           textAlign="left"
           style={{ padding: '1.5em 2em 0.8em 2em' }}
         >
-          <p style={{ fontSize: '1.4em', margin: '0' }}>
-            Manage your team {teamName}
+          <p style={{ fontSize: '1.4em', marginBottom: '0.4em' }}>
+            <span style={{ fontWeight: 'bold' }}>Team Name:</span> {teamName}
           </p>
+          <p style={{ fontSize: '1.4em', margin: '0', fontWeight: 'bold' }}>
+            Members:
+          </p>
+          <List style={{ fontSize: '1.4em', marginTop: '0.4em' }}>
+            <List.Item>
+              <List.Icon name="user circle" />
+              <List.Content>{getMe().name}</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="user circle" />
+              <List.Content>Len Beong</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="user circle" />
+              <List.Content>Soo Juen Yien</List.Content>
+            </List.Item>
+          </List>
+
+          <Segment color="violet" style={{ overflow: 'hidden' }}>
+            <p
+              style={{
+                fontSize: '1.1em',
+                fontWeight: 'bold',
+                margin: '0.2em 0em',
+              }}
+            >
+              Invite Members
+            </p>
+            <p
+              style={{
+                margin: '0.2em 0em',
+              }}
+            >
+              You can invite 1 more member.
+            </p>
+            <Input
+              placeholder="Member Email Address"
+              style={{ margin: '0.4em 0', width: '100%' }}
+              onChange={(e) => setCreateTeamName(e.target.value)}
+            />
+            <Button
+              primary
+              size="medium"
+              floated="right"
+              onClick={sendMakeTeam}
+              style={{ margin: '0.8em 0 0 0' }}
+            >
+              Invite
+            </Button>
+          </Segment>
+
+          <Message warning>
+            <Message.Header>
+              Your team has not made your project submission!
+            </Message.Header>
+            <p>
+              Please ensure you submit your project before the judging deadline
+              of 0900hrs on Day 2 of the event!
+            </p>
+          </Message>
+
+          <Button
+            size="medium"
+            style={{ display: 'block', margin: '1em auto', width: '100%' }}
+            primary
+            onClick={() => alert('Not supported yet. Sorry!')}
+          >
+            Submit your project
+          </Button>
+          <Button
+            size="medium"
+            style={{ display: 'block', margin: '1em auto', width: '100%' }}
+            negative
+          >
+            Leave Team
+          </Button>
         </Segment>
       )}
 
