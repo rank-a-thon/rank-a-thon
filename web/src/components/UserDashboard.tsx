660974e4f197c065f89299f4605884cffa9a3478
Christopher Goh <chrisgzf@gmail.com>
2020-08-02 05:50:34 +0800

    Refactor user dashboard to separate component

---
+++ web/src/components/UserDashboard.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from 'next/link';
+import { Button, Segment } from 'semantic-ui-react';
+
+const UserDashboard: React.FC<{ name: string }> = ({ name }) => (
+  <Segment basic textAlign="left" style={{ padding: '1.5em 2em' }}>
+    <p style={{ fontSize: '2.5em', marginBottom: '0' }}>Good morning,</p>
+    <p style={{ fontSize: '2.5em', fontWeight: 'bold' }}>{name}</p>
+    <p style={{ fontSize: '2em' }}>What would you like to do today?</p>
+
+    <Link href="/announcements">
+      <Button
+        as="a"
+        size="big"
+        style={{ display: 'block', margin: '1em auto', width: '100%' }}
+        color="pink"
+      >
+        View Announcements
+      </Button>
+    </Link>
+    <Link href="/team">
+      <Button
+        as="a"
+        size="big"
+        color="purple"
+        style={{ display: 'block', margin: '1em auto', width: '100%' }}
+      >
+        Edit Team
+      </Button>
+    </Link>
+    <Link href="/explore">
+      <Button
+        as="a"
+        size="big"
+        color="violet"
+        style={{ display: 'block', margin: '1em auto', width: '100%' }}
+      >
+        Explore Projects
+      </Button>
+    </Link>
+  </Segment>
+);
+
+export default UserDashboard;
--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import type { NextPage } from 'next';
-import Link from 'next/link';
-import { Button, Segment } from 'semantic-ui-react';
 import MobilePostAuthContainer from '../components/MobilePostAuthContainer';
-import { getMe } from '../data/me';
 import { makeAuthedBackendRequest } from '../lib/backend';
+import UserDashboard from '../components/UserDashboard';
 
 type PageProps = {
   getWidth?: () => number;
@@ -48,42 +46,7 @@ const DashboardLayout: NextPage<PageProps> = () => {
       judge={userType === 'judge'}
       superuser={userType === 'superuser'}
     >
-      <Segment basic textAlign="left" style={{ padding: '1.5em 2em' }}>
-        <p style={{ fontSize: '2.5em', marginBottom: '0' }}>Good morning,</p>
-        <p style={{ fontSize: '2.5em', fontWeight: 'bold' }}>{name}</p>
-        <p style={{ fontSize: '2em' }}>What would you like to do today?</p>
-
-        <Link href="/announcements">
-          <Button
-            as="a"
-            size="big"
-            style={{ display: 'block', margin: '1em auto', width: '100%' }}
-            color="pink"
-          >
-            View Announcements
-          </Button>
-        </Link>
-        <Link href="/team">
-          <Button
-            as="a"
-            size="big"
-            color="purple"
-            style={{ display: 'block', margin: '1em auto', width: '100%' }}
-          >
-            Edit Team
-          </Button>
-        </Link>
-        <Link href="/explore">
-          <Button
-            as="a"
-            size="big"
-            color="violet"
-            style={{ display: 'block', margin: '1em auto', width: '100%' }}
-          >
-            Explore Projects
-          </Button>
-        </Link>
-      </Segment>
+      <UserDashboard name={name} />
     </MobilePostAuthContainer>
   );
 };
