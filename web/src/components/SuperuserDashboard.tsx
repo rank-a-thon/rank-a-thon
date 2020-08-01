1e9602eb8e5659b8618bf610c682b326befb15e1
Christopher Goh <chrisgzf@gmail.com>
2020-08-02 06:12:00 +0800

    Add functionality to start evaluations

--- web/src/components/SuperuserDashboard.tsx
+++ web/src/components/SuperuserDashboard.tsx
@@ -1,46 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { Button, Segment } from 'semantic-ui-react';
+import { Button, Segment, Message } from 'semantic-ui-react';
+import { makeAuthedBackendRequest } from '../lib/backend';
 
-const SuperDashboard: React.FC<{ name: string }> = ({ name }) => (
-  <Segment basic textAlign="left" style={{ padding: '1.5em 2em' }}>
-    <p style={{ fontSize: '2.5em', marginBottom: '0' }}>Hi Superuser,</p>
-    <p style={{ fontSize: '2.5em', fontWeight: 'bold' }}>{name}</p>
-    <p style={{ fontSize: '2em' }}>
-      With great power comes great responsibility.
-    </p>
+const SuperDashboard: React.FC<{ name: string }> = ({ name }) => {
+  const [success, setSuccess] = useState<string>('');
+  const [failure, setFailure] = useState<string>('');
 
-    <Link href="/announcements">
+  const sendStartEval = async () => {
+    setSuccess('');
+    setFailure('');
+    try {
+      await makeAuthedBackendRequest(
+        'put',
+        'v1/ranker/start-evaluations/testevent',
+      );
+      setSuccess(
+        'Request sent successfully! Evaluations are now being generated and assigned to judges.',
+      );
+    } catch (err) {
+      setFailure(err?.response?.data?.message || err);
+    }
+  };
+
+  const sendGenerateRankings = async () => {
+    setSuccess('');
+    setFailure('');
+    try {
+      await makeAuthedBackendRequest(
+        'put',
+        'v1/ranker/calculate-team-rankings/testevent',
+      );
+      setSuccess(
+        'Request sent successfully! Rankings are now being generated, and might take a while...',
+      );
+    } catch (err) {
+      setFailure(err?.response?.data?.message || err);
+    }
+  };
+
+  return (
+    <Segment basic textAlign="left" style={{ padding: '1.5em 2em' }}>
+      <p style={{ fontSize: '2.5em', marginBottom: '0' }}>Hi Superuser,</p>
+      <p style={{ fontSize: '2.5em', fontWeight: 'bold' }}>{name}</p>
+      <p style={{ fontSize: '2em' }}>
+        With great power comes great responsibility.
+      </p>
+
       <Button
         as="a"
         size="big"
         style={{ display: 'block', margin: '1em auto', width: '100%' }}
-        color="pink"
+        color="orange"
+        onClick={sendStartEval}
       >
-        View Announcements
+        Start Evaluations
       </Button>
-    </Link>
-    <Link href="/team">
       <Button
         as="a"
         size="big"
-        color="purple"
+        color="teal"
         style={{ display: 'block', margin: '1em auto', width: '100%' }}
+        onClick={sendGenerateRankings}
       >
-        Edit Team
+        Generate Rankings
       </Button>
-    </Link>
-    <Link href="/explore">
-      <Button
-        as="a"
-        size="big"
-        color="violet"
-        style={{ display: 'block', margin: '1em auto', width: '100%' }}
-      >
-        Explore Projects
-      </Button>
-    </Link>
-  </Segment>
-);
+      <Link href="/explore">
+        <Button
+          as="a"
+          size="big"
+          color="olive"
+          style={{ display: 'block', margin: '1em auto', width: '100%' }}
+        >
+          View Generated Rankings
+        </Button>
+      </Link>
+      {failure && (
+        <Message negative style={{ margin: '0.8em 0 0 0' }}>
+          {failure}
+        </Message>
+      )}
+      {success && (
+        <Message positive style={{ margin: '0.8em 0 0 0' }}>
+          {success}
+        </Message>
+      )}
+    </Segment>
+  );
+};
 
 export default SuperDashboard;
